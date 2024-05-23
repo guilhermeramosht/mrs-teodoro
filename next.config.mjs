9930740f65607761ea9a9f20006e761f8d4a7163
guilhermeramosht <guilhermeht.ramos@gmail.com>
2024-05-23 12:03:40 -0300

    fixing pages name

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: isProd ? '/rebecca' : '',
-    assetPrefix: isProd ? '/rebecca/' : '',
+    basePath: isProd ? '/guilhermeramosht.github.io' : '',
+    assetPrefix: isProd ? '/guilhermeramosht.github.io/' : '',
     output: 'export',
 };
 
