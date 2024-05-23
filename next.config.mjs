3c60346efb3eae1ebf1a7dfce8cf43e582a80f45
guilhermeramosht <guilhermeht.ramos@gmail.com>
2024-05-23 11:56:00 -0300

    fixing repo name

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: isProd ? '/your-repo-name' : '',
-    assetPrefix: isProd ? '/your-repo-name/' : '',
+    basePath: isProd ? '/rebecca' : '',
+    assetPrefix: isProd ? '/rebecca/' : '',
     output: 'export',
 };
 
