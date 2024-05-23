87da4ebea5c84dfc6b73cdd1a16032c83cc2f383
guilhermeramosht <guilhermeht.ramos@gmail.com>
2024-05-23 12:21:13 -0300

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: isProd ? '/guilhermeramosht.github.io' : '',
-    assetPrefix: isProd ? '/guilhermeramosht.github.io/' : '',
-    output: 'export',
+    basePath: '/guilhermeramosht.github.io',
+    assetPrefix: '/guilhermeramosht.github.io/',
+    output: "export",
 };
 
 export default nextConfig;
